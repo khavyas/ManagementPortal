2058d817acc546b13d864c20b01197faaa2ae797
Rytsense-Technologies <khavyasakthi1@gmail.com>
2024-05-21 12:47:11 +0530

    Add project files.

---
+++ ManagementPortal/Data/DbContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Reflection.Emit;
+
+namespace ManagementPortal.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<UserProfile> UserProfiles { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Define relationships and constraints
+        }
+    }
+
+}
---
+++ ManagementPortal/Data/UserProfile.cs
@@ -0,0 +1,15 @@
+﻿namespace ManagementPortal.Data
+{
+    public class UserProfile
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Gender { get; set; }
+        public string MaritalStatus { get; set; }
+        public DateTime? MarriageDate { get; set; }
+        public string BloodGroup { get; set; }
+    }
+}
+
